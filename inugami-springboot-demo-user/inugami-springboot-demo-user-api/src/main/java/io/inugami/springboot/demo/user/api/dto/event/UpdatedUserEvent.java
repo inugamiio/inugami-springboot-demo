2aace5b183197b7f4b0d9495d425f393102376f6
Patrick Guillerm <pguillerm@smartwavesa.com>
2023-04-10 15:38:33 +0200

    FEAT: publish in activeMQ

--- inugami-springboot-demo-user/inugami-springboot-demo-user-api/src/main/java/io/inugami/springboot/demo/user/api/dto/event/UpdatedUserEvent.java
+++ inugami-springboot-demo-user/inugami-springboot-demo-user-api/src/main/java/io/inugami/springboot/demo/user/api/dto/event/UpdatedUserEvent.java
@@ -6,6 +6,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder(toBuilder = true)
--- inugami-springboot-demo-user/inugami-springboot-demo-user-infrastructure/src/main/java/io/inugami/springboot/demo/user/infrastructure/service/jms/UserEventPublisher.java
+++ inugami-springboot-demo-user/inugami-springboot-demo-user-infrastructure/src/main/java/io/inugami/springboot/demo/user/infrastructure/service/jms/UserEventPublisher.java
@@ -1,23 +1,45 @@
 package io.inugami.springboot.demo.user.infrastructure.service.jms;
 
+import io.inugami.maven.plugin.analysis.annotations.JmsSender;
 import io.inugami.springboot.demo.user.api.dto.UserDTO;
+import io.inugami.springboot.demo.user.api.dto.event.CreatedUserEvent;
+import io.inugami.springboot.demo.user.api.dto.event.DeletedUserEvent;
+import io.inugami.springboot.demo.user.api.dto.event.UpdatedUserEvent;
 import io.inugami.springboot.demo.user.api.service.IUserEventPublisher;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.jms.Queue;
+
+@RequiredArgsConstructor
 @Service
 public class UserEventPublisher implements IUserEventPublisher {
+
+    private final JmsTemplate jmsTemplateQueue;
+    private final Queue       createdUserQueue;
+    private final Queue       updatedUserQueue;
+    private final Queue       deletedUserQueue;
+
+
     @Override
     public void sendCreatedUser(final UserDTO user) {
-
+        jmsTemplateQueue.convertAndSend(createdUserQueue, CreatedUserEvent.builder()
+                                                                          .id(user.getId())
+                                                                          .build());
     }
 
     @Override
     public void sendUpdatedUser(final UserDTO user) {
-
+        jmsTemplateQueue.convertAndSend(updatedUserQueue, UpdatedUserEvent.builder()
+                                                                          .id(user.getId())
+                                                                          .build());
     }
 
     @Override
     public void sendDeletedUser(final UserDTO user) {
-
+        jmsTemplateQueue.convertAndSend(deletedUserQueue, DeletedUserEvent.builder()
+                                                                          .id(user.getId())
+                                                                          .build());
     }
 }
