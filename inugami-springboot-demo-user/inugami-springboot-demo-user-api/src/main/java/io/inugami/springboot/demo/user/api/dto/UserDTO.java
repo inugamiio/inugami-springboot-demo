08ae8ea034658eb73246870a2ce25b1713195729
Patrick Guillerm <pguillerm@smartwavesa.com>
2023-04-24 05:09:35 +0200

    add example for inugami 3.0.0

--- inugami-springboot-demo-user/inugami-springboot-demo-user-api/src/main/java/io/inugami/springboot/demo/user/api/dto/UserDTO.java
+++ inugami-springboot-demo-user/inugami-springboot-demo-user-api/src/main/java/io/inugami/springboot/demo/user/api/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package io.inugami.springboot.demo.user.api.dto;
 
+import io.inugami.api.documentation.Glossary;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,13 +8,16 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Glossary("utilisateur")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder(toBuilder = true)
 @Data
 public class UserDTO {
     private Long          id;
+    @Glossary("prénom")
     private String        firstname;
+    @Glossary("nom")
     private String        lastname;
     private String        email;
     private LocalDateTime createDate;
